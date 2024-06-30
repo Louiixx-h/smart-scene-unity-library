87ea414f2c83b1cda6dfc59caf79073a808bb17c
Louiixx-h <louix.sm@gmail.com>
2024-06-30 10:18:07 -0300

    feat: smart scene added

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [1.0.0] - 2024-06-30
+### First Release
+- Inserts Smart Scene
---
+++ Runtime/ISmartScene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LuisLabs.SmartScene
+{
+    public interface ISmartScene
+    {
+        public Action OnLoadingStart { get; set; }
+        public Action OnLoadingEnd { get; set; }
+        public void AddScene(string scene);
+        public void AddCurrentScene(string name);
+        public void SwitchScene(string scene);
+        public void SwitchSceneAsync(string scene);
+    }
+}
